9d7027a5c71c05215322def177846390cba02d6e
Anon <anon@example.com>
2025-07-19 10:13:05 +0100

    initial commit

---
+++ src/main/java/com/demo/dts_dev_challenge/DtsDevChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dts_dev_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtsDevChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtsDevChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/dts_dev_challenge/DtsDevChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dts_dev_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtsDevChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
