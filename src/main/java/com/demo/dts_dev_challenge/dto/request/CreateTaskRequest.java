f0ae369adfd7f1e2afb4ecdf74f44aad9db0ecbd
coding-task-anon <221681050+coding-task-anon@users.noreply.github.com>
2025-07-21 22:32:14 +0100

    Create and Edit server side validation

--- src/main/java/com/demo/dts_dev_challenge/dto/request/CreateTaskRequest.java
+++ src/main/java/com/demo/dts_dev_challenge/dto/request/CreateTaskRequest.java
@@ -3,4 +3,14 @@ package com.demo.dts_dev_challenge.dto.request;
 
 import java.time.LocalDate;
 
-public record CreateTaskRequest(String name, String description, LocalDate dueDate) {}
+public record CreateTaskRequest(String name, String description, LocalDate dueDate) {
+
+    public CreateTaskRequest{
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Required Field");
+        }
+        if (dueDate == null || dueDate().isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("Due date is required and must be in the future");
+        }
+    }
+}
--- src/main/java/com/demo/dts_dev_challenge/dto/request/EditTaskRequest.java
+++ src/main/java/com/demo/dts_dev_challenge/dto/request/EditTaskRequest.java
@@ -4,4 +4,18 @@ import com.demo.dts_dev_challenge.enums.TaskStatus;
 import java.time.LocalDate;
 
 public record EditTaskRequest(
-    String name, String description, LocalDate dueDate, TaskStatus taskStatus) {}
+    String name, String description, LocalDate dueDate, TaskStatus taskStatus) {
+
+    public EditTaskRequest {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Required Field");
+        }
+        if (dueDate == null || dueDate().isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("Due date is required and must be in the future");
+
+        }
+        if (taskStatus == null) {
+            throw new IllegalArgumentException("Task Status Required");
+        }
+    }
+}
